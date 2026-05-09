98b94a8f09dd357b3e7bb5ced2534d0367b3fe66
Fabian <114492312+LehmannFabian@users.noreply.github.com>
2026-05-09 22:27:33 +0200

    add cv

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -61,7 +61,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://lehmannfabian.github.io/src/assets/lebenslauf_fabian_lehmann_git.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://lehmannfabian.github.io/public/lebenslauf_fabian_lehmann_git.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
 skills: [
   'Python',
